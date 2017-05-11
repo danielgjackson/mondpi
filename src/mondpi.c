77d33ed387a5eacb7b7b2881445345a2c5147ead
Dan Jackson <email@danieljackson.co.uk>
2017-05-11 13:45:06 +0100

    Command-line override of the application's "DPI Awareness".

--- src/mondpi.c
+++ src/mondpi.c
@@ -12,8 +12,11 @@
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 
+static HMONITOR hMainMonitor = NULL;
+static HWND hWndMain = NULL;
 static HWND hWndEdit = NULL;
 static int scaleFactor = 100;
+static int gnCmdShow = 0;
 
 
 // Append a wide string to the log control
@@ -73,55 +76,10 @@ void UpdateFonts(void)
 	}
 	int size = 17 * scaleFactor / 100;
 	//Log("New font size: %d\r\n", size);
-	hFont = CreateFont(-size, 0, 0, 0, FW_NORMAL, 0, 0, 0, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY, FIXED_PITCH, L"Consolas");
+	hFont = CreateFont(-size, 0, 0, 0, FW_NORMAL, 0, 0, 0, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY, FIXED_PITCH, TEXT("Consolas"));
 	SendMessage(hWndEdit, WM_SETFONT, (WPARAM)hFont, TRUE);
 }
 
-
-// Handle window message
-LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-	switch (uMsg)
-	{
-		// Close window
-		case WM_DESTROY:
-			PostQuitMessage(0);
-			return 0;
-		
-		case WM_NCCREATE:
-			// Allow the non-client area to resize
-			EnableNonClientDpiScaling(hWnd);
-			break;
-
-		// Resize window
-		case WM_SIZE:
-			// Resize edit window to client area
-			if (hWndEdit != NULL)
-			{
-				WORD width = LOWORD(lParam);
-				WORD height = HIWORD(lParam);
-				MoveWindow(hWndEdit, 0, 0, width, height, 1);
-			}
-			break;	// pass through
-
-		// DPI changed
-		case WM_DPICHANGED:
-			{
-				WORD dpi = LOWORD(wParam);
-				LPRECT lprcNewScale = (LPRECT)lParam;
-				SetScaleFactor(dpi);
-				SetWindowPos(hWnd, HWND_TOP, lprcNewScale->left, lprcNewScale->top, lprcNewScale->right - lprcNewScale->left, lprcNewScale->bottom - lprcNewScale->top, SWP_NOZORDER | SWP_NOACTIVATE);
-				UpdateFonts();
-				RedrawWindow(hWnd, NULL, NULL, RDW_ERASE | RDW_INVALIDATE);
-			}
-			break;	// pass through
-
-		// Read-only edit control (not WM_CTLCOLOREDIT) has window background color
-		case WM_CTLCOLORSTATIC:
-			return (LRESULT)(HBRUSH)(COLOR_WINDOW + 1);
-	}
-	return DefWindowProc(hWnd, uMsg, wParam, lParam);
-}
 
 
 // Dump monitor data
@@ -167,7 +125,7 @@ BOOL Run(void)
 	HRESULT hr = GetProcessDpiAwareness(hProcess, &dpiAwareness);
 	if (hr != S_OK)
 	{
-		MessageBox(NULL, L"GetProcessDpiAwareness failed", (LPCWSTR)L"Notification", MB_OK);
+		MessageBox(NULL, TEXT("GetProcessDpiAwareness failed"), TEXT("Notification"), MB_OK);
 		return FALSE;
 	}
 	Log("ProcessDpiAwareness=%d (0=PROCESS_DPI_UNAWARE, 1=PROCESS_SYSTEM_DPI_AWARE, 2=PROCESS_PER_MONITOR_DPI_AWARE)\r\n", dpiAwareness);
@@ -179,40 +137,115 @@ BOOL Run(void)
 	monitorIndex = 0;
 	EnumDisplayMonitors(NULL, NULL, MonitorEnum, (LPARAM)NULL);
 
+	Log("Done\r\n");
+
 	return TRUE;
+}
+
+
+// Handle window message
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+		// Close window
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return 0;
+
+		case WM_NCCREATE:
+			// Allow the non-client area to resize
+			EnableNonClientDpiScaling(hWnd);
+			break;
+
+		// Resize window
+		case WM_SIZE:
+			// Resize edit window to client area
+			if (hWndEdit != NULL)
+			{
+				WORD width = LOWORD(lParam);
+				WORD height = HIWORD(lParam);
+				MoveWindow(hWndEdit, 0, 0, width, height, 1);
+			}
+			break;	// pass through
+
+		// DPI changed
+		case WM_DPICHANGED:
+			{
+				WORD dpi = LOWORD(wParam);
+				LPRECT lprcNewScale = (LPRECT)lParam;
+				SetScaleFactor(dpi);
+				SetWindowPos(hWnd, HWND_TOP, lprcNewScale->left, lprcNewScale->top, lprcNewScale->right - lprcNewScale->left, lprcNewScale->bottom - lprcNewScale->top, SWP_NOZORDER | SWP_NOACTIVATE);
+				UpdateFonts();
+
+				// Send to textbox?
+				SendMessage(hWndEdit, uMsg, wParam, lParam);
+
+				RedrawWindow(hWnd, NULL, NULL, RDW_ERASE | RDW_INVALIDATE);
+			}
+			break;	// pass through
+
+		// Read-only edit control (not WM_CTLCOLOREDIT) has window background color
+		case WM_CTLCOLORSTATIC:
+			return (LRESULT)(HBRUSH)(COLOR_WINDOW + 1);
+	}
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
 
 
 // Main entry point
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
+	PROCESS_DPI_AWARENESS dpiAwareness = PROCESS_PER_MONITOR_DPI_AWARE;
+
+	// Process command-line arguments
+	int argc = 0;
+	LPWSTR *argv = CommandLineToArgvW(GetCommandLineW(), &argc);
+	int paramErr = 0;
+	for (int i = 1; i < argc; i++)
+	{
+		if (_wcsicmp(L"-dpiaware", argv[i]) == 0)
+		{
+			dpiAwareness = (PROCESS_DPI_AWARENESS)_wtoi(argv[++i]);
+		}
+		else
+		{
+			paramErr++;
+		}
+	}
+	if (paramErr > 0)
+	{
+		MessageBox(NULL, TEXT("Some command-line parameters were not recognized."), TEXT("Warning"), MB_OK | MB_ICONWARNING);
+	}
+
 	// Register DPI awareness before any API call that causes the DWM to begin virtualization
-	SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE);
+	SetProcessDpiAwareness(dpiAwareness);
 
 	// Register window class
 	WNDCLASS wc = { 0 };
 	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
 	wc.lpfnWndProc = WindowProc;
 	wc.hInstance = hInstance;
-	wc.lpszClassName = L"mondpi";
+	wc.lpszClassName = TEXT("mondpi");
 	RegisterClass(&wc);
 
 	// Get main monitor DPI
 	POINT point;
 	point.x = 0;
 	point.y = 0;
-	HMONITOR hMonitor = MonitorFromPoint(point, MONITOR_DEFAULTTONEAREST);
+	hMainMonitor = MonitorFromPoint(point, MONITOR_DEFAULTTONEAREST);
 	UINT dpiX = 0, dpiY = 0;
-	GetDpiForMonitor(hMonitor, MDT_EFFECTIVE_DPI, &dpiX, &dpiY);
+	GetDpiForMonitor(hMainMonitor, MDT_EFFECTIVE_DPI, &dpiX, &dpiY);
 	SetScaleFactor(dpiX);
 
 	// Create window
+	gnCmdShow = nCmdShow;
 	int width = 640, height = 480;
-	HWND hWnd = CreateWindowEx(0, wc.lpszClassName, L"Monitor DPI", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, width * scaleFactor / 100, height * scaleFactor / 100, NULL, NULL, hInstance, NULL);
-	if (hWnd == NULL) { return 0; }
+	hWndMain = CreateWindowEx(0, wc.lpszClassName, TEXT("Monitor DPI"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, width * scaleFactor / 100, height * scaleFactor / 100, NULL, NULL, hInstance, NULL);
+	if (hWndMain == NULL) { return 0; }
 
 	// Create read-only multi-line edit control with mono-space font
-	hWndEdit = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("Edit"), TEXT(""), WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_LEFT | ES_MULTILINE | ES_AUTOVSCROLL | ES_READONLY, 0, 0, 0, 0, hWnd, NULL, NULL, NULL);
+	hWndEdit = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("Edit"), TEXT(""), WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_LEFT | ES_MULTILINE | ES_AUTOVSCROLL | ES_READONLY, 0, 0, 0, 0, hWndMain, NULL, NULL, NULL);
 	PostMessage(hWndEdit, WM_SETFOCUS, 0, 0);
 
 	// Scale fonts
@@ -223,15 +256,14 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 	Log("\r\n");
 
 	Log("--- MAIN MONITOR ---\r\n");
-	DumpMonitorInfo(hMonitor);
+	DumpMonitorInfo(hMainMonitor);
 	Log("\r\n");
 
 	// Show main window
-	ShowWindow(hWnd, nCmdShow);
+	ShowWindow(hWndMain, gnCmdShow);
 
-	// Run code
+	// Run process
 	Run();
-	Log("Done\r\n");
 
 	// Message loop
 	MSG msg = { 0 };
@@ -241,5 +273,6 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 		DispatchMessage(&msg);
 	}
 
+	LocalFree(argv);
 	return 0;
 }
